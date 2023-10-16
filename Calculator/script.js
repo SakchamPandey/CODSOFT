0e2e19c1f89f5af3c7fd85b9f0889a7fe075651f
SakchamPandey <124496405+SakchamPandey@users.noreply.github.com>
2023-10-16 11:33:08 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,23 @@
+let inputBox=document.getElementById('inputBox')
+let button=document.querySelectorAll('button')
+let string=''
+button.forEach(element =>{
+    element.addEventListener('click',(b)=>{
+        if(b.target.innerText=="="){
+            string=String(eval(string))
+            inputBox.value=string;
+        }
+        else if(b.target.innerText=='AC'){
+            string=''
+            inputBox.value=string
+        }
+        else if(b.target.innerText=='DEL'){
+            string=string.substring(0,string.length-1)
+            inputBox.value=string;
+        }
+        else{
+            string+=b.target.innerText
+            inputBox.value=string
+        }
+    })
+})
